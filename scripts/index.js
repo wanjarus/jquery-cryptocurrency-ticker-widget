cf292536f625a5c562d80c9eea9be4fb91805d1b
pkellz <pkellz@github.com>
2018-07-04 18:06:21 -0400

    Changes are now live

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,17 @@
 $(function()
 {
   $('#ticker_1').cryptoticker({
-    startIndex:0,
-    getTopCoins:20,
-    nameColor:'yellow',
-    separatorColor:'#ddd',
-    separatorWidth:10,
-    priceColor:'orange'
+    // startIndex:0,
+    // getTopCoins:20,
+    // nameColor:'yellow',
+    // separatorColor:'#ddd',
+    // priceColor:'orange'
   });
-
+  //
   $('#ticker_2').cryptoticker({
     startIndex:19,
     getTopCoins:20,
     nameColor:'#44aaff',
-    separatorColor:'red',
-    separatorWidth:10,
     priceColor:'orange'
   });
 
